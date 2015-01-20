f1a38e15e184200cc82e099d6bf720ac1b0460f9
Nathan Bubna <nathan@esha.com>
2015-01-20 10:07:02 -0700

    give form[target] attributes the ajax/SPA treatment

--- src/ajax.js
+++ src/ajax.js
@@ -15,15 +15,28 @@ var ajax = FORMx.ajax = {
         if (validate.all(form)) {
             var action = form.getAttribute('action'),
                 method = form.getAttribute('method') || 'post',
+                target = form.getAttribute('target'),
                 fn = Eventi._.resolve(action, this) || Eventi._.resolve(action, window);
             if (fn) {
                 if (typeof fn[method] === "function") {
                     fn = fn[method];
                 }
-                fn.call(form, form.xValue, e);
+                var ret = fn.call(form, form.xValue, e);
+                if (target) {
+                    ajax.target(target, ret);
+                }
             } else {
                 window.console.log('todo: actual ajax submission ', action, method);
             }
+        }
+    },
+    target: function(target, result) {
+        if (result && typeof result.then === "function") {
+            result.then(function() {
+                history.pushState(null, null, target);
+            });
+        } else if (result !== false) {
+            history.pushState(null, null, target);
         }
     }
 };
