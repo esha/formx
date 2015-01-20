dbe1276dfbcaa71a0948e6f7a0fbfa86267ce09e
Nathan Bubna <nathan@esha.com>
2015-01-20 10:38:24 -0700

    0.5.0

--- README.md
+++ README.md
@@ -11,7 +11,9 @@ Download: [formx.min.js][full-min] or [formx.js][full] [![Build Status](https://
 * 2014-08-13 [v0.2.0][] (form[ajax], textarea[flex], validate)
 * 2014-08-28 [v0.3.2][] (css for validation error message control, required-if/required-unless validations, live attributes, simpler names)
 * 2014-08-29 [v0.4.1][] (better form[ajax], misc fixes)
+* 2014-08-29 [v0.5.0][] (form[ajax] target attribute -> history.pushState)
 
 [v0.2.0]: https://github.com/esha/formx/tree/0.2.0
 [v0.3.2]: https://github.com/esha/formx/tree/0.3.2
 [v0.4.1]: https://github.com/esha/formx/tree/0.4.1
+[v0.5.0]: https://github.com/esha/formx/tree/0.5.0
--- dist/formx.js
+++ dist/formx.js
@@ -1,4 +1,4 @@
-/*! formx - v0.4.2 - 2015-01-15
+/*! formx - v0.5.0 - 2015-01-20
 * http://esha.github.io/formx/
 * Copyright (c) 2015 ESHA Research; Licensed MIT, GPL */
 
@@ -212,15 +212,28 @@ var ajax = FORMx.ajax = {
         if (validate.all(form)) {
             var action = form.getAttribute('action'),
                 method = form.getAttribute('method') || 'post',
+                target = form.getAttribute('target'),
                 fn = Eventi._.resolve(action, this) || Eventi._.resolve(action, window);
             if (fn) {
                 if (typeof fn[method] === "function") {
                     fn = fn[method];
                 }
-                fn.call(form, form.xValue, e);
+                var ret = fn.call(form, form.xValue, e);
+                if (target) {
+                    ajax.target(target, ret);
+                }
             } else {
                 window.console.log('todo: actual ajax submission ', action, method);
             }
+        }
+    },
+    target: function(target, result) {
+        if (result && typeof result.then === "function") {
+            result.then(function() {
+                history.pushState(null, null, target);
+            });
+        } else if (result !== false) {
+            history.pushState(null, null, target);
         }
     }
 };
